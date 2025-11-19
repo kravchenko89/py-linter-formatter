8fca3bf23ca760fc221fcddabef623cbff339834
kravchenko89 <vadymkravchenko.us@gmail.com>
2025-11-19 11:41:13 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -13,7 +13,8 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {"errors": [format_linter_error(error) for error in errors],
             "path": file_path,
             "status": "failed"
-            } if errors else {"errors": [], "path": file_path, "status": "passed"}
+            } if errors else {"errors": [],
+                              "path": file_path, "status": "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
